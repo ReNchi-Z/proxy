fd3147a9251415831b421755e5638d11e7834bd0
ReNchi-Z <akasunasasori69@gmail.com>
2025-02-23 20:49:55 +0700

    Update check.sh

--- check.sh
+++ check.sh
@@ -81,8 +81,8 @@ fi
 
 message+="\n🎉 _Proxy Check Successful!_"
 
-# Escape karakter khusus MarkdownV2
-escaped_message=$(echo "$message" | sed 's/[&/\#\_\*\+\-\.]/\\&/g')
+# Escape hanya karakter yang perlu di-escape untuk MarkdownV2
+escaped_message=$(echo "$message" | sed 's/[][()$&`*_\-+!]/\\&/g')
 
 # Kirim ke Telegram
 curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage" \
